c79e9b230efc46cbfd0f09220482a8a18a1fc393
ralf <chrystianralfc@gmail.com>
2024-09-05 19:00:37 -0400

    entendiendo endpoints con postman

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 const express = require('express');
 const morgan = require('morgan');
 const path  = require('path');
+
+const { connection } = require('./config/connect');
 //const authRoutes = require('./routes/auth');
 //const dashboardRoutes = require('./routes/dashboard');
 const app = express();
 
 // configuracion
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT_SERVER || 3000);
+
 // middlewares
 app.use(morgan('dev'));
 app.use(express.json());
+
 // rutas
+app.get('/employees', (req, res) => {
+  res.send('obteniendo empleados');
+});
+app.post('/employees', (req, res) => {
+  res.send('creando empleados');
+});
+app.put('/employees', (req, res) => {
+  res.send('actualizando emppleados');
+});
+app.delete('/employees', (req, res) => {
+  res.send('eliminando empleado');
+});
 
 // achivos estaticos
 app.use(express.static(path.join(__dirname, 'public')));
@@ -18,13 +34,3 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.listen(app.get('port'), () => {
   console.log('server on port ' + app.get('port'));
 });
-
-/*const PORT = 5000;
-
-//app.use(express.json());
-app.use('/', authRoutes);
-app.use('/', dashboardRoutes);
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});*/
