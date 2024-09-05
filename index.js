d7c9243c04e3785bcd66567c45caefea4f9ffd77
ralf <chrystianralfc@gmail.com>
2024-09-05 11:00:13 -0400

    configurando servidor y archivos estaticos

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express');
-const authRoutes = require('./routes/auth');
-const dashboardRoutes = require('./routes/dashboard');
+const morgan = require('morgan');
+const path  = require('path');
+//const authRoutes = require('./routes/auth');
+//const dashboardRoutes = require('./routes/dashboard');
 const app = express();
 
-const PORT = 5000;
+// configuracion
+app.set('port', process.env.PORT || 3000);
+// middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+// rutas
+
+// achivos estaticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(app.get('port'), () => {
+  console.log('server on port ' + app.get('port'));
+});
+
+/*const PORT = 5000;
 
 //app.use(express.json());
 app.use('/', authRoutes);
@@ -11,4 +27,4 @@ app.use('/', dashboardRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
-});
+});*/
