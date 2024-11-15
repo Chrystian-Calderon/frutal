27bec7c2743fb51a205f11d6fae909a68fd4d0f8
ralf <chrystianralfc@gmail.com>
2024-11-15 16:46:20 -0400

    agregando cors

--- index.js
+++ index.js
@@ -32,7 +32,9 @@ const limiter = rateLimit({
 
 // middlewares
 app.use(limiter);
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+}));
 app.use(morgan(process.env.NODE_ENV === 'production' ? 'combined' : 'dev'));
 app.use(express.json());
 app.use(errorHandler);
