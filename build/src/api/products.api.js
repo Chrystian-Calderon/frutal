2bc981830116093c74f3889e607773b2e2ca63b6
ralf <chrystianralfc@gmail.com>
2024-11-21 12:12:01 -0400

    nuevas rutas para productos

--- build/src/api/products.api.js
+++ build/src/api/products.api.js
@@ -9,3 +9,46 @@ export const getProductsNameRequest = async () =>
 
 export const getProductsAdditions = async () =>
     await axios.get(url + 'products/additions');
+
+export const createIcecreamRequest = async (data) =>
+    await axios.post(url + 'products/icecream', data);
+
+export const createAdditionRequest = async (data) =>
+    await axios.post(url + 'products/addition', data);
+
+export const getIcecreamRequest = async (id) =>
+    await axios.get(url + 'products/icecream', {
+        params: {
+            id: id
+        }
+    });
+
+export const getAdditionRequest = async (id) =>
+    await axios.get(url + 'products/addition', {
+        params: {
+            id: id
+        }
+    });
+
+export const updateIcecreamRequest = async (id, data) =>
+    await axios.patch(url + 'products/icecream', {
+        params: {
+            id: id
+        },
+        data
+    });
+    
+export const updateAdditionRequest = async (id, data) =>
+    await axios.patch(url + 'products/addition', {
+        params: {
+            id: id
+        },
+        data
+    });
+
+export const deleteProductRequest = async (id) =>
+    await axios.delete(url + 'products/delete', {
+        params: {
+            id: id
+        }
+    });
